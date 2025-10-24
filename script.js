4251159a66adc3053e3fcdf7695275b6ab1a6e67
VIMALCHINTU <158822830+VIMALCHINTU@users.noreply.github.com>
2025-10-24 13:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,24 @@
-class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
-
-  // Getter for name
-  get name() {
-    return this._name;
-  }
-
-  // Setter for name
-  set name(value) {
-    this._name = value;
-  }
-
-  // Getter for age
-  get age() {
-    return this._age;
-  }
-
-  // Setter for age
-  set age(value) {
-    this._age = value;
-  }
-
-  // Greeting method
-  greet() {
-    return `Hello, my name is ${this._name}, I am ${this._age} years old.`;
-  }
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this._jobTitle = jobTitle;
-  }
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
-  // Getter for jobTitle
-  get jobTitle() {
-    return this._jobTitle;
-  }
-
-  // Setter for jobTitle
-  set jobTitle(value) {
-    this._jobTitle = value;
-  }
-
-  jobGreet() {
-    return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
-  }
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age); // Inherit name and age from Person
+  this.jobTitle = jobTitle;
 }
+
+// Inherit Person prototype methods
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
