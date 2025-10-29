64aa4f59c256a33321b11afe4df7292ee83a7f35
VIMALCHINTU <158822830+VIMALCHINTU@users.noreply.github.com>
2025-10-29 10:11:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ Employee.prototype.constructor = Employee;
 
 // Then add Employee methods
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}`);
+  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
 };
 
 // Do not change code below this line
